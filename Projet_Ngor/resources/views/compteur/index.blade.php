209dbebed70e8f6b9aaf76e870b9b7c116766e07
Malang <ndieme16@hotmail.com>
2019-02-18 02:37:02 +0000

    first commit

---
+++ Projet_Ngor/resources/views/compteur/index.blade.php
@@ -0,0 +1,72 @@
+@extends('base')
+
+@section('main')
+<ul class="nav nav-tabs">
+<li class="nav-item">
+    <a class="nav-link active" href="{{route('home')}}">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" href="{{route('compteur.index')}}">Compteur</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{route('abonnement.index')}}">Abonnement</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{route('facture.index')}}">Facture</a>
+  </li>
+  
+</ul>
+
+
+
+<div class="row">
+
+
+<div class="col-sm-12">
+
+    <h1 class="display-3">Compteur</h1>    
+  <table class="table table-striped">
+    <thead>
+        <tr>
+          <td>ID</td>
+          <td>Numero</td>
+          <td colspan = 2>Actions</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($compteur as $compteur)
+        <tr>
+            <td>{{$compteur->id}}</td>
+            <td>{{$compteur->numero}} </td>
+            <td>
+                <a href="{{ route('compteur.edit',$compteur->id)}}" class="btn btn-primary">Edit</a>
+            </td>
+            <td>
+                <form action="{{ route('compteur.destroy', $compteur->id)}}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+<div>
+</div>
+
+<div class="col-sm-12">
+
+  @if(session()->get('success'))
+    <div class="alert alert-success">
+      {{ session()->get('success') }}  
+    </div>
+  @endif
+</div>
+
+<div>
+    <a style="margin: 19px;" href="{{ route('compteur.create')}}" class="btn btn-primary">New Compteur</a>
+</div> 
+
+
+@endsection
---
+++ Projet_Ngor/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+use App\Facture;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::resource('compteur', 'CompteurController');
+Route::resource('abonnement', 'abonnementController');
+Route::resource('facture', 'factureController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('getfacture/{id}',function (){
+   $id = request('id');
+   $item =  Facture::find($id);
+   header("Acces-Control-Allow-Origin: *");
+   return response()->json($item);
+});
